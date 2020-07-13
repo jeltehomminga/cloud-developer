20bdfa37aa0edd72e8d4b4b3786a59f3c4dde3c0
Jelte Homminga <j.homminga@hypotheekbond.nl>
2020-07-13 13:57:32 +0200

    add concat test

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.ts
@@ -1,14 +1,23 @@
 // Super Simple Unit Functions
 
+import { text } from "express";
+
 export const add = (a: number, b: number) => {
-    return a + b;
-    }
+  return a + b;
+};
 
 export const divide = (a: number, b: number) => {
-    if(b === 0) { throw new Error('div by 0') }
+  if (b === 0) {
+    throw new Error("div by 0");
+  }
 
-    return a / b;
-    }
+  return a / b;
+};
+
+export const concat = (a: string, b: string) => {
+  if (!a || !b) throw new Error("strings cannot be empty");
+  return a + b;
+};
 
 // @TODO try creating a method "concat" to concatenate two strings
 // it should take two string paramaters.
